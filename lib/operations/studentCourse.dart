441d58b9356d16d9a64b3f2345f892753dde6e0a
Suneth Udayanga <sunethcreation44@gmail.com>
2024-03-12 23:10:33 +0530

    Update studentCourse.dart

--- lib/operations/studentCourse.dart
+++ lib/operations/studentCourse.dart
@@ -1,2 +1,15 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AcademicOperation {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  Future getAcYears() async {
+    QuerySnapshot querySnapshot = await db.collection('Users').get();
+    print(querySnapshot);
+
+
+  }
+
+}
